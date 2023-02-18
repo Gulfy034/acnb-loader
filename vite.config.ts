1ae64cc91142e74cac564532be4fe011b4832148
guangzan <guangzan1999@outlook.com>
2023-02-18 14:26:38 +0800

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    emptyOutDir: true,
     lib: {
-      fileName: "loader",
-      formats: ["iife"],
-      name: "loader",
-      entry: "./src/index.ts",
+      fileName: 'loader',
+      formats: ['iife'],
+      name: 'loader',
+      entry: './src/index.ts',
     },
   },
-});
+})
